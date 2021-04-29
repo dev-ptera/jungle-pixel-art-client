4e240118926374c178931be61d4a9dd1abe43bfa
dev-ptera <dev.ptera@gmail.com>
2021-04-29 11:15:11 -0400

    Make better welcome screen

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,15 +3,18 @@
         <div class="modal">
             <div class="modal-background"></div>
             <header class="modal-header">
-                Welcome to the Gallery
+                <div class="line-1">welcome to the</div>
+                <div class="line-2">PIXELARTJUNGLE</div>
             </header>
-            <div class="wave"></div>
+            <div class="header-wave"></div>
             <section class="modal-body">
                 This is the welcome screen, the pixels are loading in the background. By the time you dismiss this,
                 the board will have loaded.
             </section>
+            <div style="display: flex; flex: 1 1 0;"></div>
+            <div class="footer-wave"></div>
             <footer class="modal-footer">
-                <button type="button" class="art-button" @click="showWelcome = false">View the Art</button>
+                <button type="button" class="art-button" @click="showWelcome = false">DO THE ART</button>
             </footer>
         </div>
     </div>
@@ -54,7 +57,8 @@ export default {
     overflow-x: auto;
     display: flex;
     flex-direction: column;
-    height: 450px;
+    max-height: 450px;
+    height: 90vh;
     width: 300px;
     border-radius: 8px;
     z-index: 5;
@@ -63,34 +67,37 @@ export default {
 
 .modal-header,
 .modal-footer {
-    padding: 16px;
     display: flex;
     background: #60c15f;
 }
 
 .modal-header {
-    font-size: 24px;
-    text-align: center;
+    padding: 16px;
     border-bottom: 1px solid #eeeeee;
     color: #ffffff;
     justify-content: space-between;
+    display: flex;
+    flex-direction: column;
+}
+.modal-header .line-1 {
     z-index: 1;
-    line-height: 72px;
-    height: 72px;
-}
-
-.modal-body {
-    color: #2a2a2e;
-    position: relative;
-    font-size: 14px;
-    padding: 16px;
-    text-align: center;
-    height: 100%;
-    margin-top: 64px;
     font-size: 16px;
+    font-weight: 200;
+    margin-bottom: 8px;
+    padding-top: 16px;
+    text-align: left;
+    margin-left: 20px;
+}
+.modal-header .line-2 {
+    z-index: 1;
+    text-align: center;
+    font-size: 24px;
+    line-height: 16px;
+    height: 36px;
+    letter-spacing: 3px;
 }
 
-.wave {
+.header-wave, .footer-wave {
     background: url("../assets/wave-2.svg") no-repeat;
     z-index: 999;
     width: 100%;
@@ -98,9 +105,27 @@ export default {
     height: 3rem;
     bottom: -.3rem;
     position: absolute;
-    top: 96px;
     background-size: cover;
     background-position: 0 0;
+}
+.header-wave {
+    top: 108px;
+}
+.footer-wave {
+    top: 335px;
+    -webkit-transform: scaleY(-1) scaleX(-1);
+    transform: scaleY(-1) scaleX(-1);
+}
+
+.modal-body {
+    color: #000000;
+    position: relative;
+    font-size: 14px;
+    padding: 16px;
+    text-align: center;
+    height: 100%;
+    margin-top: 48px;
+    font-size: 16px;
 }
 
 .modal-background {
@@ -108,12 +133,10 @@ export default {
 
 .modal-footer {
     z-index: 2;
-    border-top: 1px solid #eeeeee;
     flex-direction: column;
     justify-content: flex-end;
     background: #60c15f;
-    background: url('../assets/bg-footer.svg');
-    background-size: contain;
+    padding: 16px 32px;
 }
 .art-button {
     cursor: pointer;
@@ -121,10 +144,11 @@ export default {
     background: #438d43;
     border-radius: 4px;
     height: 36px;
-    font-weight: 600;
-    letter-spacing: 0.5px;
+    font-size: 12px;
+    font-weight: 200;
     width: 100%;
-    border: solid 2px #dcdcd0;
+    border: solid 1px #dcdcd0;
+    letter-spacing: 2px;
 }
 
 </style>
